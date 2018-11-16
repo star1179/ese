85f1618904116e91e50333b8e28c67e291d02458
DOJIN GU <rnehwls@gmail.com>
2018-11-16 18:43:59 +0900

    dj_2 anniversary

--- anniversary.c
+++ anniversary.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 #include <time.h>
+#include "anniversary.h"
 
-void select()
+void anni_select()
 {
  int choice;
+
  printf("Anniversary Menu!!\n");
  printf("1.Input the anniversary\n");
  printf("2.Print the anniverary\n");
 
+
  scanf("%d",&choice);
- switch(choice)
- {
-  case 1: input(); break;
-  case 2: show(); break;
-  default : break;
+ while(1){
+  switch(choice)
+  {
+   case 1: input(); break;
+   case 2: show(); break;
+   default : printf("1 or 2 is available\n") break;
+  }
  }
+
 }
 
-void input()
-{}
+void anni_input()
+{
+ char date[];
+ char event[];
+ printf("input the date and event [20181225]\n");
+ gets(date);
+ getchar();
+ print("input the event [meet the santa]\n");
+ gets(event);
 
-void show()
+ FILE * fd=fopen("anni.txt","a");
+ fputs(date);
+ fputs(" ");
+ fputs(event);
+
+ fclose(fp);
+}
+
+void anni_show()
 {}
--- include/anniversary.h
+++ include/anniversary.h
@@ -1,7 +1,7 @@
 #ifndef ANNIVERSARY_H
 #define ANNIVERSARY_H
-void select();
-void input();
-void show();
+void anni_select();
+void anni_input();
+void anni_show();
 
 #endif ANNIVERSARY_H
