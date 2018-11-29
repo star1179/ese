e645e1e74039f4ccfa7cad0895c6e24eeb8d1ac9
Howard <alsgh6775@naver.com>
2018-11-28 17:27:02 -0800

    MH: famous.c is upgradded

--- famous.c
+++ famous.c
@@ -3,7 +3,17 @@
 
 void Todayfamoussaying()
 {
-        char *buffer[20] = { "No pain No gain", "At the height of laughter the $
+        char *buffer[40] = { "No pain No gain",
+                             "Good words cost nothing",
+                             "Deep sorrow has no tongue",
+                             "A cold head and a warm heart",
+                             "A great obstacle to happiness is to anticipate too great a happiness",
+                             "A friend in need is a friend indeed",
+                             "open your moyth only if you are going to say is more beautiful than silence",
+                             "waste not fresh tears over old griefs",
+                             "Secret of success is constancy to purpose",
+                             "Necessity is the mother of invention",
+                             "At the height of laughter the " }
 
         //buffer check
         for (int i = 0; i < 20; i++)
