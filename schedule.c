69f74e282e10ce974c6a13a9cc4fe4dc6848efb5
DOJIN GU <rnehwls@gmail.com>
2018-12-04 01:46:55 +0900

    DJ: Schedul function revised

--- schedule.c
+++ schedule.c
@@ -5,7 +5,6 @@
 #include <errno.h>
 #include "./include/schedule.h"
 
-int txt_size = 0;
 void sche_select()
 {
  int choice;
@@ -13,7 +12,7 @@ void sche_select()
  printf("\nSchedule Management  Menu!!\n");
  printf("1.Input the Schedule\n");
  printf("2.Print the Schedule\n");
- printf("3.Delete the Schedule\n");
+ printf("3.Terminate\n");
 
  fflush(stdin);
  scanf("%d",&choice);
@@ -23,7 +22,7 @@ void sche_select()
   {
    case 1: sche_input(); break;
    case 2: sche_show(); break;
-   case 3: sche_delete(); break;
+   case 3: return; break;
    default : printf("1 or 2 is available\n"); break;
   }
 }
@@ -47,76 +46,24 @@ void sche_input()
  }
  fprintf(fp,"Date : %s", date);
  fprintf(fp," Event : %s", event);
- txt_size++;
  fclose(fp);
 }
 
 void sche_show()
 {
- FILE * fp = fopen("sche.txt","r");
+ FILE * fp = fopen("sche.txt","rt+");
  if( fp == NULL)
  {
   perror("open");
   exit(0);
  }
- char strTemp[255];
- char *pStr;
- int idx = 0;
- int i = 0;
-  while ( i++<txt_size )
-  {
-   pStr = fgets(strTemp, sizeof(strTemp),fp);
-   printf("%d. %s",idx++, strTemp);
-  }
-}
-
-void sche_delete()
-{
- FILE * fp = fopen("sche.txt","r");
- if( fp == NULL)
- {
-  perror("open");
-  exit(0);
- }
- char strTemp[255];
- char *pStr[100];
- char *del;
- int idx = 0;
- int i = 0;
- int i_idx;
- int flag = 0;
- printf("What number you want to Delete?\n");
- scanf("%d",&idx);
- getchar();
- int k = 0;
- int count = 0;
-  while ( count++ < txt_size )
-  {
-   if(k == idx)
-    continue;
-   pStr[k] = fgets(strTemp, sizeof(strTemp),fp);
-   printf("k=%d,,%s",k,pStr[k]);
-   k++;
-  }
- txt_size--;
-
+ char buf[255];
+ printf("Schdule list\n");
+ int i =0;
+ while(fgets(buf,256,fp) != NULL) printf("%d. %s",i++,buf);
  fclose(fp);
- i_idx = k-1;
-
-
- FILE * fp2=fopen("sche.txt","w");
- if( fp2 == NULL)
- {
-  perror("open");
-  exit(0);
- }
-
- for (int j=0; j<txt_size; j++)
- {
-  fprintf(fp2, "%s", pStr[j]);
- }
- fclose(fp2);
 }
+
 
 int main()
 {
