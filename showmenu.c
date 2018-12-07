3141cffaf4ad66c8dcc7a9aff9442744e071dca6
Howard <alsgh6775@naver.com>
2018-12-07 03:54:00 -0800

    MH: showmenu is updated

--- showmenu.c
+++ showmenu.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
-#include "annyversary.h"
-#include "Todayfamoussayin.h"
-#include "Todolist.h"
+#include "./include/famous.h"
+#include "./include/lotto.h"
+#include "./include/rank.h"
+#include "./include/schedule.h"
+#include "./include/todayMenu.h"
+#include "./include/todolist.h"
+#include "./include/sendlist.h"
 
 void showmenu()
 {
    int showmenu = 0;
    printf("My Daily  (made by DJ & MH\n)");
    printf("Please press button\n\n");
-   printf("1.To do list\n2.Today famous saying\n3.anniversary\n");
+   printf("1.Today Menu\n2.Today Rank(Search Rank & News Topic\n3.Today my schedule\n4.Today my todolist\n5.Today Famous saying\n6.Today Lotton/7.look on the WEB");
    printf("selection : ");
    scanf("%d", &showmenu);
 
    switch(showmenu)
    {
       case 1: show_todayMenu(); break;
-      case 2: show_searchRank(); break;
+      case 2: select_rank(); break;
       case 3: sche_select(); break;
       case 4: select_Todo(); break;
       case 5: show_famous(); break;
       case 6: show_lotto(); break;
+      case 7: send_list(); break;
+      default : printf("press any key for terminate\n"); break;
    }
 }
 
