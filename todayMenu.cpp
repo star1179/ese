7e35fd8194f318b0e01e91ad088fe79d8ed89ab0
DOJIN GU <rnehwls@gmail.com>
2018-12-10 22:58:08 +0900

    DJ: makefile updated

--- todayMenu.cpp
+++ todayMenu.cpp
@@ -9,44 +9,40 @@ using namespace std;
 extern "C"{
 void show_todayMenu()
 {
-    //system("rm cafe.txt");
-    system("python3 getHTML.py");
+   // system("rm cafe.txt");
+   // system("python3 getHTML.py");
     ifstream in("cafe.txt");
-
-    string s="";
-    string goal_start="<div class=\"year_0502_box\">";
-    string goal_end="</dlv>";
-    string end_of_file="</body></html>";
-
+    string s = "";
     int count=1;
-
     cout<<"Today Cafeteria Menu!"<<endl;
     while(1)
     {
      getline(in, s);
+
      switch(count)
      {
-      case 807:
+      case 859:
       s=s.substr(33,130);
       cout <<"1 coner: "<<s << endl;
       break;
-      case 819:
+      case 871:
       s=s.substr(33,130);
       cout <<"2 coner: "<<s << endl;
       break;
-      case 831:
+      case 883:
       s=s.substr(33,130);
       cout <<"3 coner: "<<s << endl;
       break;
-      case 843:
-      s=s.substr(33,190);
+      case 895:
+      s=s.substr(33,210);
       cout <<"4 coner: "<<s << endl;
       break;
      }
      count++;
-     if(count ==844)
+     if(count ==900)
      break;
     }
+    cout <<endl<<endl;
 }
 
 
